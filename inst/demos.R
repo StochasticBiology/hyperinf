77a86bb1aaa48d51f0d5083f16b086426e804cdb
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2025-12-17 21:51:59 +0100

    Add files via upload

---
+++ inst/demos.R
@@ -0,0 +1,38 @@
+data = matrix(c(0,0,1,
+                0,1,1,
+                1,1,1), ncol=3, nrow=3)
+# create random phylogeny
+tree = ape::rphylo(3, 1, 1)
+
+fit.mk = hyperinf(data, method="hypermk")
+fit.hhmm = hyperinf(data, method="hyperhmm")
+fit.dags = hyperinf(data, method="hyperdags")
+fit.hct = hyperinf(data, method="hypertraps")
+ggpubr::ggarrange(plot_hyperinf(fit.mk),plot_hyperinf(fit.hhmm),
+                  plot_hyperinf(fit.dags), plot_hyperinf(fit.hct),
+                  plot_hyperinf(fit.mk, plot.type="native"), plot_hyperinf(fit.hhmm, plot.type="native"),
+                  plot_hyperinf(fit.dags, plot.type="native"), plot_hyperinf(fit.hct, plot.type="native"),
+                  nrow=2, ncol=4)
+
+
+
+fit.tree.mk = hyperinf(data, tree, method="hypermk")
+fit.tree.hhmm = hyperinf(data, tree, method="hyperhmm")
+fit.tree.dags = hyperinf(data, tree, method="hyperdags")
+fit.tree.hct = hyperinf(data, tree, method="hypertraps")
+ggpubr::ggarrange(plot_hyperinf(fit.tree.mk),plot_hyperinf(fit.tree.hhmm),
+                  plot_hyperinf(fit.tree.dags), plot_hyperinf(fit.tree.hct),
+                  plot_hyperinf(fit.tree.mk, plot.type="native"), plot_hyperinf(fit.tree.hhmm, plot.type="native"),
+                  plot_hyperinf(fit.tree.dags, plot.type="native"), plot_hyperinf(fit.tree.hct, plot.type="native"),
+                  nrow=2, ncol=4)
+
+fit.tree.mk.rev = hyperinf(data, tree, method="hypermk", reversible = TRUE)
+plot_hyperinf(fit.tree.mk)
+plot_hyperinf(fit.tree.mk.rev)
+
+data = matrix(rbinom(1000, 1, 0.5), ncol=50, nrow=20)
+data = matrix(rbinom(9, 1, 0.5), ncol=3, nrow=3)
+# create random phylogeny
+tree = ape::rphylo(nrow(data), 1, 1)
+fit.rnd = hyperinf(data, method="hyperdags")
+plot_hyperinf(fit.rnd, plot.type="native")
