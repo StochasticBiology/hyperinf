763aef9732db2e1ee96e6471cfb02fdbd53974fe
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2025-12-18 09:14:55 +0100

    Updates for data plotting, PLI, data curation, etc

--- inst/demos.R
+++ inst/demos.R
@@ -5,9 +5,10 @@ data = matrix(c(0,0,1,
 tree = ape::rphylo(3, 1, 1)
 
 fit.mk = hyperinf(data, method="hypermk")
-fit.hhmm = hyperinf(data, method="hyperhmm")
+fit.hhmm = hyperinf(data, method="hyperhmm", nboot=0)
 fit.dags = hyperinf(data, method="hyperdags")
 fit.hct = hyperinf(data, method="hypertraps")
+fit.pli = hyperinf(data, method="pli")
 ggpubr::ggarrange(plot_hyperinf(fit.mk),plot_hyperinf(fit.hhmm),
                   plot_hyperinf(fit.dags), plot_hyperinf(fit.hct),
                   plot_hyperinf(fit.mk, plot.type="native"), plot_hyperinf(fit.hhmm, plot.type="native"),
@@ -17,7 +18,7 @@ ggpubr::ggarrange(plot_hyperinf(fit.mk),plot_hyperinf(fit.hhmm),
 
 
 fit.tree.mk = hyperinf(data, tree, method="hypermk")
-fit.tree.hhmm = hyperinf(data, tree, method="hyperhmm")
+fit.tree.hhmm = hyperinf(data, tree, method="hyperhmm", nboot=0)
 fit.tree.dags = hyperinf(data, tree, method="hyperdags")
 fit.tree.hct = hyperinf(data, tree, method="hypertraps")
 ggpubr::ggarrange(plot_hyperinf(fit.tree.mk),plot_hyperinf(fit.tree.hhmm),
@@ -36,3 +37,14 @@ data = matrix(rbinom(9, 1, 0.5), ncol=3, nrow=3)
 tree = ape::rphylo(nrow(data), 1, 1)
 fit.rnd = hyperinf(data, method="hyperdags")
 plot_hyperinf(fit.rnd, plot.type="native")
+
+data = matrix(c(0,0,1,
+                0,2,1,
+                1,1,1), ncol=3, nrow=3)
+
+plot_hyperinf_data(data)
+plot_hyperinf_data(data, tree)
+test.missing = hyperinf(data)
+test.missing.phylo = hyperinf(data, tree)
+ggpubr::ggarrange(plot_hyperinf(test.missing), plot_hyperinf(test.missing.phylo))
+
